88d74e3368f6c2552ce3a4a43005e1c31dc1c6ce
winhandsome <thang.nguyen30092002@hcmut.edu.vn>
2022-12-05 20:59:47 +0700

    first commit

---
+++ admin/index.php
@@ -0,0 +1,49 @@
+
+<?php include('partials/menu.php'); ?>
+
+        <!-- Main Content Section Starts -->
+        <div class="main-content">
+            <div class="wrapper">
+                <h1>Dashboard</h1>
+                <br><br>
+                <?php 
+                    if(isset($_SESSION['login']))
+                    {
+                        echo $_SESSION['login'];
+                        unset($_SESSION['login']);
+                    }
+                ?>
+                <br><br>
+
+                <div class="col-4 text-center">
+
+                    <br />
+                    Categories
+                </div>
+
+                <div class="col-4 text-center">
+
+                    <br />
+                    Foods
+                </div>
+
+                <div class="col-4 text-center">
+                    
+                    <br />
+                    Total Orders
+                </div>
+
+                <div class="col-4 text-center">
+                    
+                   
+                    <br />
+                    Revenue Generated
+                </div>
+
+                <div class="clearfix"></div>
+
+            </div>
+        </div>
+        <!-- Main Content Setion Ends -->
+
+<?php include('partials/footer.php') ?>
